fa15022d6e0f1f45415ab1357e2330c45b07fef0
Vo Long <zvothanhlong@gmail.com>
2020-07-16 22:15:27 +0700

    add store and shop reducer

---
+++ src/data/ActionCreators.js
@@ -0,0 +1,9 @@
+import { ActionTypes } from "./Types";
+import { data as phData } from "./placeholderData";
+export const loadData = (dataType) => ({
+  type: ActionTypes.DATA_LOAD,
+  payload: {
+    dataType: dataType,
+    data: phData[dataType],
+  },
+});
